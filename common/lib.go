151e33906dc1f5da4130ff1de31457d0cb78e4b1
psyomn <lethaljellybean@gmail.com>
2019-12-01 22:09:50 -0500

    Add more work for the moving pictures application!

--- common/lib.go
+++ common/lib.go
@@ -2,10 +2,16 @@ package common
 
 import (
 	"bufio"
+	"crypto/md5"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"path"
+	"path/filepath"
 )
+
+const OrgName = "ecophagy"
 
 func FileExists(filename string) bool {
 	fd, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE|os.O_EXCL, 0666)
@@ -56,6 +62,60 @@ func FileToLines(filename string) ([]string, error) {
 		}
 
 		ret = append(ret, string(line))
+	}
+
+	return ret, nil
+}
+
+func HasHome() bool { return os.Getenv("HOME") != "" }
+
+func DataPath() string {
+	return path.Join(os.Getenv("HOME"), ".config", OrgName)
+}
+
+func PathExists(path string) bool {
+	_, err := os.Stat(path)
+
+	if os.IsNotExist(err) {
+		return false
+	}
+
+	return true
+}
+
+// FileToMd5Sum will calculate the hash of a particular file
+// contents. The intended use is for quick checks rather than
+// secure.
+func FileToMd5Sum(path string) (string, error) {
+	fh, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer fh.Close()
+
+	hash := md5.New()
+	if _, err := io.Copy(hash, fh); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", hash.Sum(nil)), nil
+}
+
+func FileList(dirpath string) ([]string, error) {
+	var ret []string
+
+	err := filepath.Walk(dirpath,
+		func(path string, info os.FileInfo, err error) error {
+			if info.IsDir() {
+				return nil
+			}
+
+			ret = append(ret, path)
+			return nil
+		})
+
+	if err != nil {
+		return nil, err
 	}
 
 	return ret, nil
---
+++ mvpic/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"os"
+	"path"
+	"path/filepath"
+
+	"github.com/psyomn/ecophagy/common"
+)
+
+const (
+	AppName = "mvpic"
+	advice  = "https://www.kaggle.com/rounakbanik/the-movies-dataset"
+)
+
+func dataPath() string {
+	return path.Join(common.DataPath(), AppName)
+}
+
+func setup() {
+	moviePath := dataPath()
+
+	if !common.PathExists(moviePath) {
+		fmt.Println("you might want to download data into", moviePath)
+		fmt.Println("the data can be found here: ", advice)
+		err := os.MkdirAll(moviePath, 0766)
+		if err != nil {
+			fmt.Println(err)
+		}
+		return
+	}
+
+	err := checkFiles()
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func checkFiles() error {
+	expectedFiles := map[string]string{
+		"credits.csv":         "d62a1d7d652d324bebee68782f664fc9",
+		"keywords.csv":        "7d0f5123e11917fa1bea011448e5f73d",
+		"links.csv":           "b9b8fe775557e10e35418410499775cf",
+		"links_small.csv":     "917eddf52079d6ce5c6cfd63b17515f7",
+		"movies_metadata.csv": "42bf3ef8c208a01a4776955875978b1e",
+		"ratings.csv":         "f640a181f6fa0b2e3294f786fa350ccc",
+		"ratings_small.csv":   "8864480f98416ccecaf17aa5263bbea1",
+	}
+
+	files, err := common.FileList(dataPath())
+	if err != nil {
+		return err
+	}
+
+	for _, file := range files {
+		justFileName := filepath.Base(file)
+		expectedHash := expectedFiles[justFileName]
+
+		actualHash, err := common.FileToMd5Sum(file)
+		if err != nil {
+			return err
+		}
+
+		if actualHash != expectedHash {
+			errorMsg := fmt.Sprintf("%s bad file hash: expected: %s, got: %s",
+				file, expectedHash, actualHash)
+			return errors.New(errorMsg)
+		}
+	}
+
+	return nil
+}
+
+func main() {
+	if !common.HasHome() {
+		panic("you need to have a home in order to run this")
+	}
+
+	setup()
+
+	flag.Parse()
+
+	fmt.Println("do stuff")
+}
