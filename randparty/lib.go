453b9b272359820a3d3c9df3e4bec415b32f1ed4
Simon Symeonidis <lethaljellybean@gmail.com>
2020-05-25 01:28:12 -0400

    Make randparty create things in xdg friendly locations

--- randparty/lib.go
+++ randparty/lib.go
@@ -3,22 +3,49 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"math/rand"
 	"os"
+	"path"
 	"time"
 
 	"github.com/psyomn/ecophagy/common"
 )
 
-func initialize() {
-	rand.Seed(time.Now().Unix())
+const (
+	AppName = "randparty"
+
+	configFileName = "config.txt"
+
+	defaultConfig = `nouns.txt https://raw.githubusercontent.com/taikuukaits/SimpleWordlists/master/Wordlist-Nouns-All.txt
+verbs.txt https://raw.githubusercontent.com/taikuukaits/SimpleWordlists/master/Wordlist-Verbs-All.txt
+adjct.txt https://raw.githubusercontent.com/taikuukaits/SimpleWordlists/master/Wordlist-Adjectives-All.txt
+`
+)
+
+func initialize() { rand.Seed(time.Now().Unix()) }
+
+func dataPath() string { return path.Join(common.DataPath(), AppName) }
+
+func configFile() string { return path.Join(dataPath(), configFileName) }
+
+func createDefaultConfig() error {
+	err := os.MkdirAll(dataPath(), 0755)
+	if err != nil {
+		return err
+	}
+
+	return ioutil.WriteFile(configFile(), []byte(defaultConfig), 0644)
 }
 
 func main() {
 	initialize()
 
-	config, err := readSpaceConfig("config.txt")
+	config, err := common.ReadSpaceConfig(configFile())
 	if err != nil {
+		log.Println("error:", err)
+
 		fmt.Println("you need to supply a config file")
 		fmt.Println("each line in the config file is a filename")
 		fmt.Println("associated to a url. example: ")
@@ -26,6 +53,26 @@ func main() {
 		fmt.Println("nouns.txt www.someplace.com/nouns.txt")
 		fmt.Println("verbs.txt www.someplace.com/verbs.txt")
 		fmt.Println("adjct.txt www.someplace.com/adjct.txt")
+		fmt.Println("")
+
+		fmt.Print("Would you like to create a default config? [y/n]: ")
+		reader := bufio.NewReader(os.Stdin)
+		text, err := reader.ReadString('\n')
+		if err != nil {
+			panic(err)
+		}
+
+		if text == "n" || text == "no" {
+			return
+		}
+
+		err = createDefaultConfig()
+		if err != nil {
+			panic(err)
+		}
+
+		log.Println("should download appropriate files next run")
+
 		return
 	}
 
--- randparty/utils.go
+++ randparty/utils.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
 	"math/rand"
-	"os"
 )
 
 func sampleArray(arr []string) string {
@@ -14,36 +11,4 @@ func sampleArray(arr []string) string {
 
 	index := rand.Intn(len(arr))
 	return arr[index]
-}
-
-func readSpaceConfig(filename string) (map[string]string, error) {
-	// space configuration is a key value file, whose key values are
-	// separated by a space, on each line
-
-	fd, err := os.Open(filename)
-	if err != nil {
-		return nil, err
-	}
-	defer fd.Close()
-
-	reader := bufio.NewReader(fd)
-	var line []byte
-	ret := map[string]string{}
-
-	for {
-		line, _, err = reader.ReadLine()
-		if err != nil {
-			break
-		}
-
-		parts := bytes.Split(line, []byte(" "))
-
-		if len(parts) != 2 {
-			panic(string(parts[0]))
-		}
-
-		ret[string(parts[0])] = string(parts[1])
-	}
-
-	return ret, nil
 }
