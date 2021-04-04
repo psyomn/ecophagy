8dd375d11c36d1299eb7433d7e40e037a6cccdb4
Simon Symeonidis <lethaljellybean@gmail.com>
2021-04-04 17:32:26 -0400

    [randparty] Fix lints and other performance tweaks

--- randparty/utils.go
+++ randparty/utils.go
@@ -1,12 +1,30 @@
 package main
 
-import "math/rand"
+// TODO: consider moving this to root common, and having common
+// capabilities for random sampling of arrays.
+
+import (
+	mrand "math/rand"
+	"time"
+)
+
+func initializeRandEngine() {
+	mrand.Seed(time.Now().Unix())
+}
 
 func sampleArray(arr []string) string {
 	// tempting to use unsafe here, but I know that this will be
 	// used on a windows machine, and not too sure how that will
 	// behave there.
 
-	index := rand.Intn(len(arr))
+	// This complains of not very strong crypto rand, but we really
+	// don't care for an application like this -- we just want random
+	// words from a list, and that's good enough. One could argue that
+	// maybe the sequences that are going to show up will not be as
+	// unique, but again, I don't think it's worth investing on this
+	// subject...
+	//
+	// nolint
+	index := mrand.Intn(len(arr))
 	return arr[index]
 }
