09f1c734054290038b2cfbc1fa1e5bbb8f3fecf0
Simon Symeonidis <lethaljellybean@gmail.com>
2021-04-22 23:22:01 -0400

    [tinystory] Add tests that use the fixtures for tests

--- tinystory/lib/parser_test.go
+++ tinystory/lib/parser_test.go
@@ -6,7 +6,13 @@ import (
 	"github.com/psyomn/ecophagy/common"
 )
 
-const fixture = "../stories/simple.json"
+const (
+	base                    = "../stories"
+	fixture                 = base + "/simple.json"
+	tinystoryFixtureSimple  = base + "/simple.tinystory"
+	tinystoryFixtureSmall01 = base + "/small.tinystory"
+	tinystoryFixtureSmall02 = base + "/small-2.tinystory"
+)
 
 func TestTinyStoryParserTitle(t *testing.T) {
 	data, err := common.FileToBytes(fixture)
@@ -22,5 +28,33 @@ func TestTinyStoryParserTitle(t *testing.T) {
 
 	if document == nil {
 		t.Fatalf("document must not be nil")
+	}
+}
+
+func TestTinyStoryFormat(t *testing.T) {
+	type testCase struct {
+		name string
+		path string
+	}
+
+	tcs := []testCase{
+		{"simple", tinystoryFixtureSimple},
+		{"small", tinystoryFixtureSmall01},
+		{"small-2", tinystoryFixtureSmall02},
+	}
+
+	for index := range tcs {
+		t.Run(tcs[index].name, func(t *testing.T) {
+			// nolint:scopelint // succint tests good
+			doc, err := ParseTinyStoryFormatFile(tcs[index].path)
+
+			if doc == nil {
+				t.Error("docs should not be nil")
+			}
+
+			if err != nil {
+				t.Errorf("error not nil: %s", err.Error())
+			}
+		})
 	}
 }
